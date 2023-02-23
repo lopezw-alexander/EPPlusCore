cf1667646c0ad24865e4dd4244fe95b3628f7c79
AllianceDeveloper <Developer@allianceairproducts.com>
2023-02-23 11:20:32 -0800

    Add project files.

---
+++ EEPlusCore/Sparkline/ExcelSparkline.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace OfficeOpenXml.Sparkline
+{
+    /// <summary>
+    /// Represents a single sparkline within the sparkline group
+    /// </summary>
+    public class ExcelSparkline : XmlHelper
+    {
+        internal ExcelSparkline(XmlNamespaceManager nsm, XmlNode topNode) : base(nsm, topNode)
+        {
+            SchemaNodeOrder = new string[] { "f", "sqref" };
+        }   
+        const string _fPath = "xm:f";
+        /// <summary>
+        /// The datarange
+        /// </summary>
+        internal ExcelAddressBase RangeAddress
+        {
+            /*get
+            {
+                return new ExcelAddressBase(GetXmlNodeString(_fPath));
+            }
+            internal*/
+            set
+            {
+                //SetXmlNodeString(_fPath, value.FullAddress);
+
+                if (value is ExcelNamedRange)
+                    SetXmlNodeString(_fPath, (value as ExcelNamedRange).Name);
+                else
+                    SetXmlNodeString(_fPath, value.FullAddress);
+            }
+        }
+
+        /// <summary>
+        /// Get the data range address.
+        /// </summary>
+        /// <param name="namedRangeCol">workbook or worksheet Names</param>
+        /// <returns></returns>
+        internal ExcelAddressBase GetRangeAddress(ExcelNamedRangeCollection namedRangeCol)
+        {
+            string addrOrName = GetXmlNodeString(_fPath);
+            if (namedRangeCol.ContainsKey(addrOrName)) //ExcelNamedRange
+                return namedRangeCol[addrOrName];
+            else
+                return new ExcelAddressBase(addrOrName);
+        }
+
+        const string _sqrefPath = "xm:sqref";
+        /// <summary>
+        /// Location of the sparkline
+        /// </summary>
+        public ExcelCellAddress Cell
+        {
+            get
+            {
+                return new ExcelCellAddress(GetXmlNodeString(_sqrefPath));
+            }
+            internal set
+            {
+                SetXmlNodeString("xm:sqref", value.Address);
+            }
+        }
+        public override string ToString()
+        {
+            //return Cell.Address + ", " +RangeAddress.Address;
+            return Cell.Address + ", " + GetXmlNodeString(_fPath);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# Continuous Version of EPPlus Free Edition 4.5.3.3
+**The [JanKallman/EPPlus](https://github.com/JanKallman/EPPlus) repository has been archived by the owner. So I fork it to my repository and continue to maintain the source code.**
+
+---
+**This repository has moved to https://github.com/EPPlusSoftware/EPPlus.** 
+
+**The code in this archive represents the final version of EPPlus under LGPL. There will be no more activity here.**  
+
+EPPlus will from version 5 switch license from **LGPL** to [Polyform Noncommercial 1.0.0]( https://polyformproject.org/licenses/noncommercial/1.0.0/) license.  
+With the new license EPPlus is still free to use in some cases, but will require a commercial license to be used in a commercial business.
+
+More information on the license change on [our website]( https://www.epplussoftware.com)
+***
+Create advanced Excel spreadsheets using .NET, without the need of interop.
+
+EPPlus is a .NET library that reads and writes Excel files using the Office Open XML format (xlsx). 
+EPPlus has no dependencies other than .NET.
+ 
+## EPPlus supports:
+* Cell Ranges 
+* Cell styling (Border, Color, Fill, Font, Number, Alignments) 
+* Data validation 
+* Conditional formatting 
+* Charts 
+* Pictures 
+* Shapes 
+* Comments 
+* Tables 
+* Pivot tables 
+* Protection 
+* Encryption 
+* VBA 
+* Formula calculation 
+* Many more... 
+
+## Overview
+This project started with the source from ExcelPackage. It was a great project to start from.
+It had the basic functionality needed to read and write a spreadsheet.
+Advantages over other:
+EPPlus uses dictionaries to access cell data, making performance a lot better.
+Complete integration with .NET 
+
+## Support
+All support is currently referred to [Stack overflow](https://stackoverflow.com/questions/tagged/epplus). 
+A tutorial is available in the wiki and the sample project can be downloaded with each version. 
+The old site at [Codeplex](http://epplus.codeplex.com) also contains material that can be helpful. 
+Bugs and new feature requests can be added to the issues tracker. 
+
+## License
+The project is licensed under the GNU Library General Public License (LGPL). 
